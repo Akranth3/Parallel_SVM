b8186e77d3efe4f4737d5bd0c2949faf34926f6d
Akranth3 <akranthreddy@gmail.com>
2024-03-31 11:27:07 +0530

    Added empty files and Data directory

---
+++ code/Parallel_SVM.c
@@ -0,0 +1,14 @@
+/*This Program has the Parallel implementation of SVM Algorithm*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <omp.h>
+
+
+
+
+int main(){
+    printf("Running the Parallel SVM code\n");
+    return 0;
+}
---
+++ code/Serial_SVM.c
@@ -0,0 +1,13 @@
+/*This Program has the serial implementation of SVM Algorithm*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+
+
+
+int main(){
+    printf("Running the serial SVM code\n");
+    return 0;
+}
