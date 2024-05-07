56834c709e7fb3817fe4acec184c9dd10c6a11f4
Akranth3 <akranthreddy@gmail.com>
2024-05-07 13:07:03 +0530

    pushing serial code date 7th may

--- code/Serial_SVM.c
+++ code/Serial_SVM.c
@@ -40,20 +40,68 @@ void read_csv(char *filename, int num_data_points, int num_features, double data
 }
 
 
+
 int main(){
     printf("Running the serial SVM code\n");
     
     char filename[100];
-    sprintf(filename, "../Data/IRIS/Iris.csv");
+    sprintf(filename, "../Data/Two_class/data.csv");
 
-    int num_points = 150;
-    int num_features = 4;
-    double data[num_points][num_features];
+    int num_points = 200;
+    int num_features = 2; 
+    double data[num_points][num_features+1]; // 2 features and 1 label
 
     //populating the array with the data
     read_csv(filename, num_points, num_features, data);
-
+    printf("Data read successfully\n");
+    printf("the first data point is %f %f %f\n", data[0][0], data[0][1], data[0][2]);
     
+    double w[num_features-1];
+    double b = 0.0;
+    double alpha = 0.001;
+    int num_iterations = 1000;
+    double lamda = 0.01;
+
+    //initializing the weights and bias
+    for(int i=0; i<num_features-1; i++){
+        w[i] = 0;
+    }
+
+    for(int i=0; i<num_iterations; i++){
+        for(int j=0; j<num_points; j++){
+            double y = data[j][num_features];
+            double sum = 0;
+            for(int k=0; k<num_features-1; k++){
+                sum += w[k]*data[j][k];
+            }
+            double z = y*(sum - b);
+            if(z < 1){
+                for(int k=0; k<num_features-1; k++){
+                    w[k] = w[k] - alpha*(2*lamda*w[k] - y*data[j][k]);
+                }
+                b = b - alpha*y;
+            }
+            else{
+                for(int k=0; k<num_features-1; k++){
+                    w[k] = w[k] - alpha*(2*lamda*w[k]);
+                }
+            }
+        }
+
+    }
+    FILE *file = fopen("../model/Two_class/model.csv", "w");
+    if(file == NULL){
+        printf("Error: File not found\n");
+        exit(1);
+    }
+    fprintf(file,"w,b\n");
+    for(int i=0; i<num_features-1; i++){
+        fprintf(file, "%f,%f\n", w[i], b);
+    }
+    fclose(file);
+
+
+
 
     return 0;
 }
---
+++ code/plot.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+# Read the data
+file_name = '../data/Two_class/data.csv'
+data = pd.read_csv(file_name)
+
+model_location = '../model/Two_class/model.csv'
+model = pd.read_csv(model_location)
+x_ax = np.linspace(data.iloc[:, 1].min(), data.iloc[:, 1].max(), 100)
+y_ax = x_ax * model.iloc[0, 0] + model.iloc[0,1]
+# Plot the data
+plt.scatter(data.iloc[:,1 ], data.iloc[:, 2], c=data.iloc[:, 3], cmap='coolwarm')
+plt.plot(x_ax, y_ax, color='black')
+plt.xlabel('X1')
+plt.ylabel('X2')
+plt.title('Data')
+plt.savefig('../Plots/Two_class.png')
